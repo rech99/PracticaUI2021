0d0283e7c7003b73e98ee37b4e11c07b3fa319cb
Alumno <cesarrgomez@hotmail.com>
2021-08-24 16:14:21 -0700

    Initial Commit

---
+++ Practica UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practica UI
+//
+//  Created by Alumno on 8/24/21.
+//  Copyright © 2021 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
